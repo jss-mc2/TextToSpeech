9b651b1203199448e89c454148812b292b21d9c5
Jason Rich Darmawan <63768126+jasonrichdarmawan@users.noreply.github.com>
2023-06-18 16:15:43 +0700

    Initial Commit

---
+++ TextToSpeech/TextToSpeechApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextToSpeechApp.swift
+//  TextToSpeech
+//
+//  Created by Jason Rich Darmawan Onggo Putra on 18/06/23.
+//
+
+import SwiftUI
+
+@main
+struct TextToSpeechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
